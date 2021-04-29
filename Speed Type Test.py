56b60b4d4662666a44fb658af2cfdd1b05d52346
Md-Ammar <ammaratif559@gmail.com>
2021-04-29 16:18:55 +0530

    removed unnecessary parts reorganized.

--- Speed Type Test.py
+++ Speed Type Test.py
@@ -14,29 +14,9 @@ font = pygame.font.SysFont('chiller', 25, True)
 run = True
 typing = True
 
-s = " "
+para = [""]
+line = 0
 key_delay = 0
-
-def textbox(event, x, y):
-    global font, s, key_delay
-    if pygame.event.poll().type != 0:
-        print(pygame.event.poll())
-
-    if event.type == pygame.KEYDOWN:
-        key = str(pygame.key.name(event.key))
-        print(key)
-        if key == "backspace":
-            s = s[0:len(s) - 1]
-        if key == "space":
-            s += "_"
-        if len(key) == 1:# and key.upper() != s[len(s) - 1]:
-            print(s[len(s) - 1], key)
-            s += key
-            s = s.upper()
-
-    txt = font.render(s, 1, (0, 0, 0))
-    win.blit(txt, (x, y))
-
 
 def refresh_win():
     win.fill((0, 200, 200))
@@ -44,8 +24,9 @@ def refresh_win():
     pygame.draw.rect(win, (255, 255, 255), (0.2 * w, 0.2 * h, 500, 100))
     pygame.draw.rect(win, (0, 0, 0), (0.2 * w, 0.2 * h, 500, 100), 2)
 
-    txt = font.render(s, 1, (0, 0, 0))
-    win.blit(txt, (0.2 * w, 0.2 * h))
+    for Line in para:
+        txt = font.render(Line, 1, (0, 0, 0))
+        win.blit(txt, (0.2 * w, 0.2 * h + para.index(Line) * 20))
 
     pygame.display.update()
 
@@ -59,17 +40,20 @@ while run:
             w = win.get_width()
             h = win.get_height()
             pygame.display.set_mode(event.dict['size'], HWSURFACE | DOUBLEBUF | RESIZABLE)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            run = False
-        elif event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             key = str(pygame.key.name(event.key))
+            if key == "escape":
+                run = False
             if key == "backspace":
-                s = s[0:len(s) - 1]
+                para[line] = para[line][0:len(para[line]) - 1]
             if key == "space":
-                s += "_"
+                para[line] += " "
             if len(key) == 1:  # and key.upper() != s[len(s) - 1]:
-                s += key
-                s = s.upper()
+                if len(para[line]) > 20:
+                    para.append("")
+                    line += 1
+                para[line] += key
+                para[line] = para[line].upper()
 
     refresh_win()
 
